cb61ca77d335b6c53d66b71c67fcf330d02d2ce0
Geoffrey Collins <82984876+GeoffreyCollins@users.noreply.github.com>
2023-05-17 13:17:35 -0600

    New File that Implements Structs

--- main.rs
+++ main.rs
@@ -1,21 +1,13 @@
-use rand::Rng;
 fn main() {
-    let mut rng = rand::thread_rng();
-
-    let number1 = rng.gen_range(1..100);
-    let number2 = rng.gen_range(1..100);
-    let addition = number1 + number2;
-    let multiplication = number1 * number2;
-
-    if addition < 100 {
-        println!("The sum of {number1} and {number2} is less than 100.");
-    } else {
-        println!("The sum is over 100!");
-    }
-
-    if multiplication < 100 {
-        println!("The product of {number1} and {number2} is less than 100.");
-    } else {
-        println!("The product is over 100!");
-    }
+    let triangle = AreaOfTriangle{
+        base: 5, height: 10
+    };
+    
+    println!("The area of the triangle is: {:#?}", triangle.base * triangle.height);
+}
+#[derive(Debug)]
+struct AreaOfTriangle
+{
+    base: u32,
+    height: u32,
 }
